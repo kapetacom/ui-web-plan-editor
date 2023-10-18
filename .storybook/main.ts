81e81b8f8430164989d244d54a81b9e57444ff51
Jacob Wejendorp <jacob@wejendorp.dk>
2023-10-18 08:22:36 +0200

    build: bump storybook and fix themes

---
+++ .storybook/main.ts
@@ -0,0 +1,58 @@
+import type { StorybookConfig } from '@storybook/react-webpack5';
+import lessPreprocessor from 'less';
+
+const config: StorybookConfig = {
+    stories: ['../stories/**/*.stories.tsx'],
+
+    framework: {
+        name: '@storybook/react-webpack5',
+        options: {},
+    },
+
+    addons: [
+        '@storybook/addon-essentials',
+        {
+            name: '@storybook/addon-styling-webpack',
+
+            options: {
+                rules: [
+                    {
+                        test: /\.css$/,
+                        sideEffects: true,
+                        use: [
+                            require.resolve('style-loader'),
+                            {
+                                loader: require.resolve('css-loader'),
+                                options: {},
+                            },
+                        ],
+                    },
+                    {
+                        test: /\.less$/,
+                        sideEffects: true,
+                        use: [
+                            require.resolve('style-loader'),
+                            {
+                                loader: require.resolve('css-loader'),
+                                options: {},
+                            },
+                            require.resolve('less-loader'),
+                        ],
+                    },
+                ],
+            },
+        },
+    ],
+
+    webpackFinal: async (config, { configType }) => {
+        if (config.module && config.module.rules) {
+            config.module.rules.push({
+                test: /\.ya?ml$/,
+                use: ['json-loader', 'yaml-loader'],
+            });
+        }
+        return config;
+    },
+};
+
+export default config;
---
+++ .storybook/preview.tsx
@@ -0,0 +1,68 @@
+import { Preview } from '@storybook/react';
+import { configure } from 'mobx';
+import { MemoryRouter } from 'react-router-dom';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { lightTheme, darkTheme } from '@kapeta/style';
+import { useMemo } from 'react';
+
+configure({
+    enforceActions: 'always',
+    computedRequiresReaction: true,
+    reactionRequiresObservable: true,
+    observableRequiresReaction: true,
+    disableErrorBoundaries: true,
+});
+
+// Add your theme configurations to an object that you can
+// pull your desired theme from.
+const THEMES = {
+    // TODO: fix theme json to match mui theme
+    light: createTheme(lightTheme as any),
+    dark: createTheme(darkTheme as any),
+};
+
+export const withMuiTheme = (Story, context) => {
+    // The theme global we just declared
+    const { theme: themeKey } = context.globals;
+
+    // only recompute the theme if the themeKey changes
+    const theme = useMemo(() => THEMES[themeKey] || THEMES['light'], [themeKey]);
+
+    return (
+        <div>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <MemoryRouter>
+                    <Story />
+                </MemoryRouter>
+            </ThemeProvider>
+        </div>
+    );
+};
+
+const preview: Preview = {
+    parameters: {
+        actions: { argTypesRegex: '^on[A-Z].*' },
+    },
+    globalTypes: {
+        theme: {
+            name: 'Theme',
+            title: 'Theme',
+            description: 'Theme for our components',
+            defaultValue: 'light',
+            toolbar: {
+                icon: 'paintbrush',
+                dynamicTitle: true,
+                items: [
+                    { value: 'light', left: '☀️', title: 'Light mode' },
+                    { value: 'dark', left: '🌙', title: 'Dark mode' },
+                ],
+            },
+        },
+    },
+    decorators: [withMuiTheme],
+};
+
+export default preview;
+
+window['__DEV__'] = true;
