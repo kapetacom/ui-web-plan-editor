d57fec7ca7b7566b99871d8635c9abea849f3f8d
Henrik Hofmeister <henrik@newdawn.dk>
2024-02-06 14:02:35 +0100

    license

--- src/planner/utils/connectionUtils.test.ts
+++ src/planner/utils/connectionUtils.test.ts
@@ -1,3 +1,7 @@
+/**
+ * Copyright 2023 Kapeta Inc.
+ * SPDX-License-Identifier: BUSL-1.1
+ */
 import { describe, test, expect } from '@jest/globals';
 import { BlockDefinition, Connection, Dimensions, Plan } from '@kapeta/schemas';
 import { AssetInfo } from '../../types';
